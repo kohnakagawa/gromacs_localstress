8ab96618f842d4d1fb7780f04cd75832e10504b2
kohnakagawa <tsunekou1019@gmail.com>
2016-10-02 15:52:39 +0900

    test util is added.

---
+++ tests/local_stress/stress/ave_lstress.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+tensortools -f localstress.dat0 --integ x --oformat bin -o outputx.dat0
+tensortools -f outputx.dat0 --integ y --oformat bin -o outputxy.dat0
+tensortools -f outputxy.dat0 --integ z --oformat bin -o outputxyz.dat0
+tensortools -f outputxyz.dat0 --oformat txt -o out.txt
+
+# tensortools -f localstress.dat0 --oformat txt -o out.txt
+
+g_energy_LS -f ener.edr <<EOF
+31 0
+EOF
+
+cat out.txt
