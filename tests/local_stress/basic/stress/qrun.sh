876f0cb971c998104903ca095b2ef008d715743e
kohnakagawa <tsunekou1019@gmail.com>
2016-10-12 11:13:32 +0900

    rename

---
+++ tests/local_stress/basic/stress/qrun.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mdrun_LS -s ../lstress.tpr -rerun ../trj_centered10.trr -lsfd ccfd -lscont all -localsgrid 0.1 -lsgridx 1 -lsgridy 1 -lsgridz 83
+# mdrun_LS -s ../lstress.tpr -rerun ../trj_centered10.trr -lsfd fcd -lscont all -localsgrid 0.1 -lsgridx 1 -lsgridy 1 -lsgridz 83
+# mdrun_LS -s ../lstress.tpr -rerun ../trj_centered10.trr -lsfd hdlm -lscont angles -localsgrid 0.1 -lsgridx 1 -lsgridy 1 -lsgridz 83
+# mdrun_LS -s ../lstress.tpr -rerun ../trj_centered10.trr -lsfd hdgm -lscont angles -localsgrid 0.1 -lsgridx 1 -lsgridy 1 -lsgridz 83
